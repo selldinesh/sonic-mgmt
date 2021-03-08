50317ae1ff74836b409ab8cd6965b96a6d34c848
Dinesh <dinesh.sellappan@keysight.com>
2021-03-08 07:03:59 +0000

    indent

--- tests/ixia/bgp/test_bgp_convergence.py
+++ tests/ixia/bgp/test_bgp_convergence.py
@@ -6,15 +6,15 @@ from tests.common.fixtures.conn_graph_facts import (
     conn_graph_facts, fanout_graph_facts)
 import pytest
 
-@pytest.mark.parametrize('multipath',[5])
+@pytest.mark.parametrize('multipath',[2])
 @pytest.mark.parametrize('convergence_test_iterations',[1])
 def test_bgp_convergence(snappi_api,
-                        duthost,
-                        tgen_ports,
-                        conn_graph_facts,
-                        fanout_graph_facts,
-                        multipath,
-                        convergence_test_iterations):
+                         duthost,
+                         tgen_ports,
+                         conn_graph_facts,
+                         fanout_graph_facts,
+                         multipath,
+                         convergence_test_iterations):
 
     """
     Topo:
@@ -45,7 +45,7 @@ def test_bgp_convergence(snappi_api,
     """
     #convergence_test_iterations and multipath values can be modified as per user preference
     run_bgp_convergence_test(snappi_api,
-                            duthost,
-                            tgen_ports,
-                            convergence_test_iterations,
-                            multipath)
+                             duthost,
+                             tgen_ports,
+                             convergence_test_iterations,
+                             multipath)
