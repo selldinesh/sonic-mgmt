f186b5ad14f3ce86662f9237029246ddd136d892
selldinesh <dinesh.sellappan@keysight.com>
2024-05-20 20:03:39 +0000

    Adding support for RDMA cases for  pfcQueueGroupSize of 4

--- tests/snappi_tests/variables.py
+++ tests/snappi_tests/variables.py
@@ -52,3 +52,14 @@ config_set = {
 dut_ip_start = '20.0.1.1'
 snappi_ip_start = '20.0.1.2'
 prefix_length = 24
+
+
+pfcQueueGroupSize = 8  # can have values 4 or 8
+pfcQueueValueDict = {0: 0,
+                     1: 1,
+                     2: 0,
+                     3: 3,
+                     4: 2,
+                     5: 0,
+                     6: 1,
+                     7: 0}
